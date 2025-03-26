bdcf5b4ccf2002169a7a9cc37e286434cd360933
SabakaPimpek <kubaboczka1@gmail.com>
2025-03-26 14:09:47 +0100

    first page version

---
+++ index.js
@@ -0,0 +1,4 @@
+function openPdfInNewWindow() {
+    // Otwiera okno z PDF bez paska adresu i innych elementów
+    window.open('pan-tadeusz.pdf', 'PDFWindow', 'width=800,height=600,toolbar=no,scrollbars=yes,location=no,status=no,resizable=yes');
+}
